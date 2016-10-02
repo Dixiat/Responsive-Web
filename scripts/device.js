44b049bab431e835aa16010e927c4d15c0d94208
Harry Field <yangqm1019@gmail.com>
2016-10-02 18:57:48 +0800

    测试网格布局在不同设备上的应用；添加响应式字体实例

--- scripts/device.js
+++ scripts/device.js
@@ -4,4 +4,5 @@ let preferences = window.Preferences || {};
 if(preferences) {
     let container = document.querySelector('.container');
     preferences.deviceType.isMobile && container.setAttribute('device-type', 'mobile');
+    preferences.deviceType.isPC && container.setAttribute('device-type',"pc");
 }
